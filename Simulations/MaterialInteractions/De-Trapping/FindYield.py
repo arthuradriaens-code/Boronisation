d05a8f5ffdace8116571a05c19d66136c54d0515
arthuradriaens-code <arthur99.adriaens@gmail.com>
2025-03-13 16:44:28 +0100

    damn it works :]

--- Simulations/MaterialInteractions/De-Trapping/FindYield.py
+++ Simulations/MaterialInteractions/De-Trapping/FindYield.py
@@ -1,31 +1,71 @@
 import numpy as np
 from libRustBCA import *
 from scripts.materials import *
+import matplotlib.pyplot as plt
 
-#example ions array like [[energy1,angle1],[energy2,angle2],...]
-#the example is with 10000 ions but 100000 also doesn't take to much time
-N = 1000 #amount of particles to simulate
-Z = 1 #atoomnummer inkomend deeltje
-Zimp = 8 #atoomnummer vuiligheid
-m = 1.0072766 #in amu
-mimp = 15.999 #in amu
-Ecut = 3.5#cutoff energie inkomend deeltje
-Esur = 4.84#surface binding energie inkomend deeltje
+def Simulate(E,theta,N=10000):
+    #example ions array like [[energy1,angle1],[energy2,angle2],...]
+    ion = hydrogen #inkomend deeltje
+    impurity = oxygen
 
-ux = list(np.ones(N))  #xvec
-uy = list(np.zeros(N)) #yvec
-uz = list(np.zeros(N)) #zvec
-Z1 = list(np.ones(N)*Z) 
-m1 = list(np.ones(N)*m) 
-Ec1 = list(np.ones(N)*Ecut) 
-Es1 = list(np.ones(N)*Esur) 
-Z2 = list([5,Zimp]) 
-m2 = list([10.811,mimp]) 
-Ec2 = list([5.0,0.2]) 
-Es2 = list([5.76,0.2]) 
-Eb2 = list([0.0,0.0]) 
-n2 = list([0.115864,0.01]) #density in #/angstroms^3
+    angle = theta
+    #In RustBCA's geometry, +x -> into the surface
+    ux = np.cos(angle*np.pi/180.)*np.ones(N)
+    uy = np.sin(angle*np.pi/180.)*np.ones(N)
+    uz = np.zeros(N)
 
-energies = list(np.random.default_rng().uniform(0.0, 20.0, N)) #0-20eV
+    Zmat = [5,oxygen['Z']]
+    mmat = [10.811,oxygen['m']]
+    Ecmat =[5.0,0.2]
+    Esmat =[5.76,0.2]
+    Ebmat =[0.0,0.0]
+    nmat = [[0.115864,0.05]] #density in #/angstroms^3
+    Dmat = [100]
 
-output = compound_bca_list_py(ux,uy,uz,energies,Z1,m1,Ec1,Es1,Z2,m2,Ec2,Es2,n2,Eb2)
+    energies = np.ones(N)*E
+
+    output,incident,stopped = compound_bca_list_1D_py(
+            ux,uy,uz,energies,[ion['Z']]*N,
+            [ion['m']]*N,[ion['Ec']]*N,[ion['Es']]*N,
+            Zmat,mmat,Ecmat,Esmat,Ebmat,nmat,Dmat) 
+    # output is (NX9 list of lists of f64) each row represents an output particle
+    # (implanted,sputtered, or reflected)
+    # elke rij: [Z, m (amu), E (eV), x(angstrom), y(angstrom), z(angstrom), ux, uy, uz]
+    # incident: true als incident ion, false als uit target
+    output = np.array(output)
+    Z = output[:,0]
+    m = output[:,1]
+    E = output[:,2]
+    x = output[:,3]
+    y = output[:,4]
+    z = output[:,5]
+    ux = output[:,6]
+    uy = output[:,7]
+    uz = output[:,8]
+
+    #print(N - len(Z[np.where(np.array(stopped)==False)]))
+    impurities = len(np.where(Z[np.where(np.array(stopped)==False)]==8)[0])
+    nboron = len(np.where(Z[np.where(np.array(stopped)==False)]==5)[0])
+    implanted = len(np.where(Z[np.logical_and(incident, stopped)]==ion['Z'])[0])
+    return {'sputtered impurities':impurities,
+            'sputtered boron':nboron,
+            'implanted ions':implanted}
+
+if __name__ == '__main__':
+    angles = np.linspace(0.0001,89.0001,90)
+    impurities = []
+    base = []
+    implanted = []
+    for angle in angles:
+        result = Simulate(300,angle,10000)
+        impurities.append(result['sputtered impurities'])
+        base.append(result['sputtered boron'])
+        implanted.append(result['implanted ions'])
+    #print("sputtered impurities: " + str(result['sputtered impurities']))
+    #print("sputtered Boron: "+ str(result['sputtered boron']))
+    #print("implanted Hydrogen: "+ str(result['implanted ions']))
+    plt.plot(angles,impurities,label='impurities')
+    plt.plot(angles,base,label='boron')
+    plt.plot(angles,implanted,label='implanted')
+    plt.legend()
+    plt.show()
---
+++ Simulations/MaterialInteractions/De-Trapping/test.py
@@ -0,0 +1,180 @@
+from libRustBCA import *
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+import os
+#This should allow the script to find materials and formulas from anywhere
+sys.path.append(os.path.dirname(__file__)+'/../scripts')
+sys.path.append('scripts')
+from materials import *
+from formulas import *
+import time
+
+def main():
+
+    #test rotation to and from RustBCA coordinates
+
+    #nx, ny, nz is the normal vector (out of surface)
+    nx = -np.sqrt(2)/2
+    ny = -np.sqrt(2)/2
+    nz = 0.0
+
+    #ux, uy, uz is the particle direction (simulation coordinates)
+    ux = 1.0
+    uy = 0.0
+    uz = 0.0
+
+    print(f'Before rotation: ({ux}, {uy}, {uz})')
+    ux, uy, uz = rotate_given_surface_normal_py(nx, ny, nz, ux, uy, uz)
+    print(f'After rotation: ({ux}, {uy}, {uz})')
+    ux, uy, uz = rotate_back_py(nx, ny, nz, ux, uy, uz)
+    print(f'After rotation back: ({ux}, {uy}, {uz})')
+
+    #After rotating and rotating back, effect should be where you started (minus fp error)
+    assert(abs(ux - 1.0) < 1e-6)
+    assert(abs(uy) < 1e-6)
+    assert(abs(uz) < 1e-6)
+
+    #scripts/materials.py has a number of potential ions and targets
+    ion = helium
+    ion['Eb'] = 0.0
+    target = tungsten
+
+    #tungsten Eb = 0 is a 'worst-case' - accepted literature value is 3 eV
+    target['Eb'] = 3.0
+
+    energy = 2500.0 #eV
+    angle = 0.0 #degrees
+    num_samples = 1000
+    #For automatic control, libRustBCA offers two helpful functions for the sputtering yield and reflection coefficients
+    Y = sputtering_yield(ion, target, energy, angle, num_samples)
+
+    print(f'Sputtering yield for {ion["symbol"]} on {target["symbol"]} at {energy} eV is {Y} at/ion. Yamamura predicts { np.round(yamamura(ion, target, energy),3)} at/ion.')
+
+    R_N, R_E = reflection_coefficient(ion, target, energy, angle, num_samples)
+    print(f'Particle reflection coefficient for {ion["symbol"]} on {target["symbol"]} at {energy} eV is {R_N}. Thomas predicts {np.round(thomas_reflection(ion, target, energy), 3)}.')
+    print(f'Energy reflection coefficient for {ion["symbol"]} on {target["symbol"]} at {energy} eV is {R_E}')
+
+    R_N, R_E = compound_reflection_coefficient(ion, [target, ion], [target['n'], 0.1*target['n']], energy, angle, num_samples)
+    print(f'Particle reflection coefficient for {ion["symbol"]} on {ion["symbol"]}x{target["symbol"]} where x=0.1 at {energy} eV is {R_N}. Thomas predicts {np.round(thomas_reflection(ion, target, energy), 3)}.')
+    print(f'Energy reflection coefficient for {ion["symbol"]}x{target["symbol"]} where x=0.1 at {energy} eV is {R_E}')
+
+    vx0 = 1e5
+    vy0 = 1e5
+    vz0 = 0.0
+    vx1, vy1, vz1 = reflect_single_ion_py(ion, target, vx0, vy0, vz0)
+
+    print(f'(vx, vy, vz) before reflection: ({vx0}, {vy0}, {vz0})')
+    print(f'(vx, vy, vz) after reflection: ({vx1}, {vy1}, {vz1})')
+
+    #For smooth distributions and good statistics, you should use at least 10k ions
+    number_ions = 10000
+
+    #1 keV is above the He on W sputtering threshold of ~150 eV
+    energies_eV = 1000.0*np.ones(number_ions)
+
+    #Working with angles of exactly 0 is problematic due to gimbal lock
+    angle = 0.0001
+
+    #In RustBCA's 0D geometry, +x -> into the surface
+    ux = np.cos(angle*np.pi/180.)*np.ones(number_ions)
+    uy = np.sin(angle*np.pi/180.)*np.ones(number_ions)
+    uz = np.zeros(number_ions)
+
+    print(f'Running RustBCA for {number_ions} {ion["symbol"]} ions on {target["symbol"]} at {energies_eV[0]/1000.} keV...')
+    print(f'This may take several minutes.')
+
+    start = time.time()
+    #Note that simple_bca_list_py expects number densities in 1/Angstrom^3
+    output = np.array(simple_bca_list_py(energies_eV, ux, uy, uz, ion['Z'],
+        ion['m'], ion['Ec'], ion['Es'], target['Z'], target['m'],
+        target['Ec'], target['Es'], target['n']/10**30, target['Eb']))
+    stop = time.time()
+    delta_time = stop - start
+
+    print('Simulation complete. Processing data...')
+
+    Z = output[:, 0]
+    m = output[:, 1]
+    E = output[:, 2]
+    x = output[:, 3]
+    y = output[:, 4]
+    z = output[:, 5]
+    ux = output[:, 6]
+    uy = output[:, 7]
+    uz = output[:, 8]
+
+    #For the python bindings, these conditionals can be used to distinguish
+    #between sputtered, reflected, and implanted particles in the output list
+    sputtered = output[np.logical_and(Z == target['Z'], E > 0), :]
+    reflected = output[np.logical_and(Z == ion['Z'], x < 0), :]
+    implanted = output[np.logical_and(Z == ion['Z'], x > 0), :]
+
+    plt.figure(1)
+    plt.title(f'Sputtered {target["symbol"]} Energy Distribution')
+    plt.xlabel('E [eV]')
+    plt.ylabel('f(E) [A.U.]')
+    plt.hist(sputtered[:, 2], bins=100, density=True, histtype='step')
+
+    plt.figure(2)
+    plt.title(f'Implanted {ion["symbol"]} Depth Distribution')
+    plt.xlabel('x [A]')
+    plt.ylabel('f(x) [A.U.]')
+    plt.hist(implanted[:, 3], bins=100, density=True, histtype='step')
+
+    thomas = thomas_reflection(ion, target, energies_eV[0])
+    yamamura_yield = yamamura(ion, target, energies_eV[0])
+
+    print('Data processing complete.')
+    print(f'RustBCA Y: {len(sputtered[:, 0])/number_ions} Yamamura Y: {yamamura_yield}')
+    print(f'RustBCA R: {len(reflected[:, 0])/number_ions} Thomas R: {thomas}')
+    print(f'Time per ion: {delta_time/number_ions*1e3} us/{ion["symbol"]}')
+
+    #Next up is the layered target version. I'll add a 50 Angstrom layer of W-H to the top of the target.
+
+    #1 keV is above the He on W sputtering threshold of ~150 eV
+    energies_eV = 1000.0*np.ones(number_ions)
+
+    #Working with angles of exactly 0 is problematic due to gimbal lock
+    angle = 0.0001
+
+    #In RustBCA's 0D geometry, +x -> into the surface
+    ux = np.cos(angle*np.pi/180.)*np.ones(number_ions)
+    uy = np.sin(angle*np.pi/180.)*np.ones(number_ions)
+    uz = np.zeros(number_ions)
+
+    print(f'Running RustBCA for {number_ions} {ion["symbol"]} ions on {target["symbol"]} with hydrogenated layer at {energies_eV[0]/1000.} keV...')
+    print(f'This may take several minutes.')
+    #Not the different argument order; when a breaking change is due, this will
+    #be back-ported to the other bindings as well for consistency.
+    output, incident, stopped = compound_bca_list_1D_py(
+        ux, uy, uz, energies_eV, [ion['Z']]*number_ions,
+        [ion['m']]*number_ions, [ion['Ec']]*number_ions, [ion['Es']]*number_ions, [target['Z'], 1.0], [target['m'], 1.008],
+        [target['Ec'], 1.0], [target['Es'], 1.5], [target['Eb'], 0.0], [[target['n']/10**30, target['n']/10**30], [target['n']/10**30, 0.0]], [50.0, 1e6]
+    )
+
+    output = np.array(output)
+
+    Z = output[:, 0]
+    m = output[:, 1]
+    E = output[:, 2]
+    x = output[:, 3]
+    y = output[:, 4]
+    z = output[:, 5]
+    ux = output[:, 6]
+    uy = output[:, 7]
+    uz = output[:, 8]
+
+    plt.figure(3)
+    plt.title(f'Implanted {ion["symbol"]} Depth Distribution with 50A {target["symbol"]}-H layer')
+    plt.xlabel('x [A]')
+    plt.ylabel('f(x) [A.U.]')
+    heights, _, _ = plt.hist(x[np.logical_and(incident, stopped)], bins=100, density=True, histtype='step')
+    plt.plot([50.0, 50.0], [0.0, np.max(heights)*1.1])
+    plt.gca().set_ylim([0.0, np.max(heights)*1.1])
+
+    plt.show()
+
+if __name__ == '__main__':
+    main()
+
